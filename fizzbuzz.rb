50acf5308c2d13da92ff0c64664669a444b8ef77
jocular-inspector-4517 <shivbhatt@hotmail.com>
2019-02-25 03:14:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 && int % 5 == 0
-    "Fizzbuzz"
+if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
   elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
