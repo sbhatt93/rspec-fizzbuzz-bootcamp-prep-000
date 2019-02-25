a67f13689fdf2690f19e8b3bea4b34487cbae765
jocular-inspector-4517 <shivbhatt@hotmail.com>
2019-02-25 02:49:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  else int 
-    "Fizz"
+  else int % 3 && 5 == 0 
+    "Fizzbuzz"
   elsif int % 5 == 0
     "Buzz"
   else int % 3 && 5 == 0
